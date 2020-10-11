1525a71ed9d6c5fff0927126bbbfd50fbffb07e6
pritamganesh <72689468+pritamganesh@users.noreply.github.com>
2020-10-11 18:48:04 +0545

    site init

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const url = "https://api.github.com/repos/pritamganesh/pritamganesh.github.io/contents/images";
+const container = document.querySelector(".photos-container");
+const loading = document.querySelector(".lds-heart");
+async function GetImages() {
+    const response = await fetch(url);
+    const images = await response.json();
+    loading.style.display = "none";
+    return images;
+}
+
+GetImages().then(images => {
+    images.reverse();
+    images.forEach(image => {
+        var content = `<div class="col-6 col-md-6 col-lg-4" data-aos="fade-up">
+        <a href=${image.download_url} class="d-block photo-item" data-fancybox="gallery">
+           <img src=${image.download_url} alt="Image" class="img-fluid">
+           <div class="photo-text-more">
+              <span class="icon icon-search"></span>
+           </div>
+        </a>
+     </div>`;
+        container.innerHTML += content;
+    });
+});
